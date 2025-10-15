604ff49bd9fefeb2263a82e8311acdbdd0ae688e
Daniilberd <daniilberd789962d@gmail.com>
2025-10-15 18:43:37 +0300

    Add files via upload

---
+++ lab16.js
@@ -0,0 +1,19 @@
+
+let myName = "Данііл";
+const myAge = 17;
+let isStudent = true;
+
+console.log(typeof myName); // "string"
+console.log(typeof myAge); // "number"
+console.log(typeof isStudent); // "boolean"
+
+let person = {
+    name: myName,
+    age: myAge
+};
+
+let hobbies = ["малювання", "читання", "програмування"];
+
+console.log(person);
+console.log(hobbies);
+
